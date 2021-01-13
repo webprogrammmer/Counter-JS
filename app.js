85862f5652561ab024ec5230f7552756f344e7e5
webprogrammmer <webprogrammmerr@gmail.com>
2021-01-13 18:53:58 +0500

    Add new files

---
+++ app.js
@@ -0,0 +1,32 @@
+let count = 0;
+
+const value = document.getElementById('value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }
+        else if (styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if (count > 0) {
+            value.style.color = 'green';
+        }
+        if (count < 0) {
+            value.style.color = 'red';
+        }
+        if (count === 0) {
+            value.style.color = '#222';
+        }
+        // console.log(e.currentTarget.classList);
+        value.textContent = count;
+    })
+    // console.log(item);
+})
+
+// console.log(btns);
